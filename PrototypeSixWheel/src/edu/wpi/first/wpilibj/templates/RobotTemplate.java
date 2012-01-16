92da324e845f572991fc6cce8550cb2a871dd93a
unknown <Gillie@Gillie-VAIO.(none)>
2012-01-16 11:15:22 -0500

    six wheel prototype tank drive

---
+++ PrototypeSixWheel/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+ package edu.wpi.first.wpilibj.templates;
+
+    import edu.wpi.first.wpilibj.IterativeRobot;
+    import edu.wpi.first.wpilibj.Joystick;
+    import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class RobotTemplate extends IterativeRobot {
+        
+    Joystick leftStick, rightStick;
+        RobotDrive dt; 
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+
+         leftStick = new Joystick(1);
+         rightStick = new Joystick(2);
+         dt = new RobotDrive(5,1,4,2);
+         
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+            double yLeft, yRight;
+            yLeft = -leftStick.getRawAxis(2);
+            yRight = -rightStick.getRawAxis(2);
+            dt.tankDrive(yLeft,yRight); 
+    }
+    
+}
