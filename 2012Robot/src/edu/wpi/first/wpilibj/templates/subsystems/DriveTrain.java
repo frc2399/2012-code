decc206dd89fe463085f063d89774a5cf8ef286f
Lauren Dierker <ldierker14@hb.edu>
2012-01-21 16:34:18 -0500

    Drive Train

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/DriveTrain.java
@@ -2,17 +2,38 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+        
 /**
  *
  */
 public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+    Joystick rightStick, leftStick;
+    RobotDrive dt; 
+    
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        rightStick = new Joystick(1);
+        leftStick = new Joystick(2);
+        dt = new RobotDrive(5,4,1,2);
+    }
+    
+    public void autonomousPeriod() {
+
+}
+
+    
+    public void teleopPeriod() {
+        double yLeft, yRight;
+        yLeft = -leftStick.getRawAxis(2);
+        yRight = -rightStick.getRawAxis(2);
+        dt.tankDrive (yLeft, yRight);
     }
 }
+    
 
