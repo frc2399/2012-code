c33c03636b90aa6eee4a8bb07ee37edc7290bdc5
unknown <Gillie@Gillie-VAIO.(none)>
2012-01-21 11:24:30 -0500

    empty command-based code classes set up

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -5,11 +5,15 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
- package edu.wpi.first.wpilibj.templates;
+package edu.wpi.first.wpilibj.templates;
 
-    import edu.wpi.first.wpilibj.IterativeRobot;
-    import edu.wpi.first.wpilibj.Joystick;
-    import edu.wpi.first.wpilibj.RobotDrive;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.templates.commands.CommandBase;
+import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
+import edu.wpi.first.wpilibj.templates.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,36 +23,45 @@
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
-        
-    Joystick leftStick, rightStick;
-        RobotDrive dt; 
+
+    Command autonomousCommand;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+        // instantiate the command used for the autonomous period
+        autonomousCommand = new ExampleCommand();
 
-         leftStick = new Joystick(2);
-         rightStick = new Joystick(1);
-         dt = new RobotDrive(5,1,4,2);
-         
+        // Initialize all subsystems
+        CommandBase.init();
+    }
+
+    public void autonomousInit() {
+        // schedule the autonomous command (example)
+        autonomousCommand.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
 
+    public void teleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to 
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		autonomousCommand.cancel();
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-            double yLeft, yRight;
-            yLeft = -leftStick.getRawAxis(2);
-            yRight = -rightStick.getRawAxis(2);
-            dt.tankDrive(yLeft,yRight); 
+        Scheduler.getInstance().run();
     }
-    
 }
