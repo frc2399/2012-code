37f77bdb83765cf6defc8cd15aa57fb9b8c9e44c
Shannon Phillips <shannonphillips@shannon-phillipss-macbook.local>
2012-01-21 15:31:44 -0500

    new feeder code

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Feeder.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Feeder.java
@@ -2,6 +2,10 @@
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
 
 /**
  *
@@ -10,9 +14,24 @@ public class Feeder extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
+    
+    Jaguar feedMotor = new Jaguar(RobotMap.feedMotor);
+    Joystick feedButt = new Joystick(RobotMap.feedButt);
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
+       // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+        
+        //default is set to zero
+        feedMotor.set(0);
+    }
+    
+    
+    public void teleopCommand(){
+        if(feedButt.getRawButton(1) == true){
+            feedMotor.set(0.5);
+        } else {
+            feedMotor.set(0);
+        }
     }
 }
 
