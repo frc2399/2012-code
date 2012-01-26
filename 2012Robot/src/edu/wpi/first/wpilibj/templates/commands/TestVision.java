d40f6e348c10289f3ab4ba7f64805ee8e3906fc2
unknown <Gillie@Gillie-VAIO.(none)>
2012-01-25 20:46:53 -0500

    TestVision updated with better rectangle finding and prints particle centers

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/TestVision.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/TestVision.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.image.RGBImage;
 public class TestVision extends CommandBase {
 
     AxisCamera camera;
+    CriteriaCollection cc;
 
     public TestVision() {
         // Use requires() here to declare subsystem dependencies
@@ -26,20 +27,36 @@ public class TestVision extends CommandBase {
     // Called just before this Command runs the first time
     protected void initialize() {
         camera = AxisCamera.getInstance();
+        cc = new CriteriaCollection();
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 20, 400, false);
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 15, 400, false);
+
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         try {
             ColorImage image = camera.getImage();
-            BinaryImage greenThreshold = image.thresholdRGB(0,151,198,255,0,255);
+            BinaryImage greenThreshold = image.thresholdRGB(0, 187, 189, 255, 0, 225);
             BinaryImage convexHullImage = greenThreshold.convexHull(false);
-            
-            
-        } catch (AxisCameraException ex){
-        
-        }
-        catch (NIVisionException ex) {
+            BinaryImage noSmallParticles = convexHullImage.removeSmallObjects(false, 7);
+            BinaryImage foundParticles = noSmallParticles.particleFilter(cc);
+
+            /* what to do with ParticleAnalysisReport:
+             * want center of particles, number of particles
+             * we'll just print it for now
+             */
+            ParticleAnalysisReport[] reports = foundParticles.getOrderedParticleAnalysisReports();  // get list of results
+            System.out.println("Number of particles: " + reports.length);
+            for (int i = 0; i < reports.length; i++) {                                // print results
+                ParticleAnalysisReport r = reports[i];
+                double centerX = r.boundingRectLeft + 0.5 * r.boundingRectWidth;
+                double centerY = r.boundingRectTop + 0.5 * r.boundingRectHeight;
+                System.out.println("Particle: " + i + " center: (" + centerX + "," + centerY +")");
+            }
+
+        } catch (AxisCameraException ex) {
+        } catch (NIVisionException ex) {
         }
 
     }
