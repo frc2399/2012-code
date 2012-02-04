b71eac141a1372a37f948a26f8857d656b5746f0
Lauren Dierker <ldierker14@hb.edu>
2012-02-04 11:58:22 -0500

    2d arrays

--- DashboardExtension/src/edu/wpi/first/smartdashboard/camera/ImageProcessing.java
+++ DashboardExtension/src/edu/wpi/first/smartdashboard/camera/ImageProcessing.java
@@ -21,42 +21,45 @@ import java.util.ArrayList;
  */
 public class ImageProcessing extends WPICameraExtension {
 
-    @Override
-    public WPIImage processImage(WPIColorImage rawImage) {
-       
+    public double[][] getCenters(WPIColorImage rawImage) {
+
         //find color thresholds: red(0,151), green(198,255), blue(0,255)
         WPIBinaryImage redBinary = rawImage.getRedChannel().getThresholdInverted(151);
         WPIBinaryImage greenBinary = rawImage.getGreenChannel().getThreshold(198);
         WPIBinaryImage blueBinary = rawImage.getBlueChannel().getThreshold(0);
-       
+
         // contains the pixels that show up in all three of the other images
         WPIBinaryImage finalBinary = blueBinary.getAnd(redBinary).getAnd(greenBinary);
-        
+
         finalBinary.dilate(6);
         finalBinary.erode(5);
-        
+
         //use contours' getHeight and getWidth to find particles that are big enough to be considered
         WPIContour[] contours = finalBinary.findContours();
-        ArrayList<WPIContour> finalContours = new ArrayList<WPIContour>(); 
-        
-        for (WPIContour c : contours){
-           if (c.getHeight() > 15 && c.getWidth() > 20){
-               finalContours.add(c);
-           }
+        ArrayList<WPIContour> finalContours = new ArrayList<WPIContour>();
+
+        for (WPIContour c : contours) {
+            if (c.getHeight() > 15 && c.getWidth() > 20) {
+                finalContours.add(c);
+            }
         }
-        
+
         // find the centers of each of the contours
         double[] contourCentersX = new double[finalContours.size()];
-        for (int i = 0; i < finalContours.size(); i++){
-           contourCentersX[i] = finalContours.get(i).getX() + 0.5*finalContours.get(i).getWidth();
+        for (int i = 0; i < finalContours.size(); i++) {
+            contourCentersX[i] = finalContours.get(i).getX() + 0.5 * finalContours.get(i).getWidth();
         }
-double[] contourCentersY = new double[finalContours.size()];
+        double[] contourCentersY = new double[finalContours.size()];
+        for (int i = 0; i < finalContours.size(); i++) {
+            contourCentersY[i] = finalContours.get(i).getY() + 0.5 * finalContours.get(i).getHeight();
+        }
+        // Puts the center in a 2D array 
+        double[][] contourCenters = new double[finalContours.size()][2];
         for (int i = 0; i < finalContours.size(); i++){
-           contourCentersY[i] = finalContours.get(i).getY() + 0.5*finalContours.get(i).getHeight();
-        }            
+            contourCenters[i][0]= contourCentersX[i];
+            contourCenters[i][1]= contourCentersY[i];
+        }
         
-        
-     
-        return rawImage.getGreenChannel();
+        return contourCenters;
     }
 }
