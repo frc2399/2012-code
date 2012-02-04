8e744ddde8af9d6cac19e2e0bfdc37bbe83728d0
Jessie Adkins <zaheelee@gmail.com>
2012-02-04 10:55:30 -0500

    Go Test take 3.6

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/Go.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/Go.java
@@ -18,7 +18,7 @@ public class Go extends CommandBase {
     public Go(double dist) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-        distToGo = dist - 4.0;
+        distToGo = dist;
     }
 
     // Called just before this Command runs the first time
