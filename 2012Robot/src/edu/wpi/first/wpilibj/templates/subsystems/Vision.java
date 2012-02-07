9d77112a660200e245f7dadfdea1cab7a0b9dc4b
Gillie Rosen <puella.noctis@gmail.com>
2012-02-06 20:45:17 -0500

    aweyghdgrwsfyj

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Vision.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/Vision.java
@@ -15,7 +15,7 @@ public class Vision extends Subsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new TestVision());
-        setDefaultCommand(new Aim());
+      //  setDefaultCommand(new Aim());
     }
     
 }
