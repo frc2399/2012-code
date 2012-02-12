009f7bfd840b25ec5789a55d3fb753fd8f2c383b
Jessie Adkins <zaheelee@gmail.com>
2012-02-12 16:13:04 -0500

    Shmacker Testing 1.2

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/ShmackDown.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/ShmackDown.java
@@ -20,6 +20,7 @@ public class ShmackDown extends CommandBase {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+        rampSmacker.setSpeed(0.25);
     }
 
     // Make this return true when this Command no longer needs to run execute()
