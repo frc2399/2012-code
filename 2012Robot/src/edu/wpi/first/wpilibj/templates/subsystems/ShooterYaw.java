c18ac081f463d139af8336a377b78ae6cfe8dd96
Gillie Rosen <puella.noctis@gmail.com>
2012-02-19 12:35:21 -0500

    why is yaw moving

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterYaw.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/subsystems/ShooterYaw.java
@@ -1,4 +1,3 @@
-
 package edu.wpi.first.wpilibj.templates.subsystems;
 
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
@@ -7,6 +6,8 @@ import edu.wpi.first.wpilibj.templates.RobotMap;
 import edu.wpi.first.wpilibj.templates.commands.Aim;
 import edu.wpi.first.wpilibj.templates.commands.ManYaw;
 import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  * A Subsystem extending PIDSubystem that contains methods for controlling the ShooterYaw
  * @author Gillie, Lauren, and Emma
@@ -14,21 +15,23 @@ import edu.wpi.first.wpilibj.CANJaguar;
 public class ShooterYaw extends PIDSubsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-    
+
     public static final int MaxAngle = 120;
     public static final int MinAngle = -120;
     private Encoder encoder = new Encoder(RobotMap.yawEncoderA, RobotMap.yawEncoderB);
     private CANJaguar yawMotor;
-    
-    public ShooterYaw(){
-        super(0.24,0.09,0.0);                                         
+
+    public ShooterYaw() {
+        super(0.24, 0.09, 0.0);
         setSetpointRange(MaxAngle, MinAngle);
         setSetpoint(0);
         //positive is couterclockwise as seen from above
         encoder.setDistancePerPulse(0.0833750);
         encoder.start();
         enable();
-        
+
+        SmartDashboard.putDouble("YawSetpoint", getSetpoint());
+        SmartDashboard.putDouble("encoderAngle", encoder.getDistance());
         try {
             yawMotor = new CANJaguar(RobotMap.yawMotor);
         } catch (Exception e) {
@@ -39,24 +42,23 @@ public class ShooterYaw extends PIDSubsystem {
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
-       // setDefaultCommand(new Aim(1));
+        // setDefaultCommand(new Aim(1));
     }
-    
-    protected double returnPIDInput(){
+
+    protected double returnPIDInput() {
         return -encoder.getDistance();
         //for sensor
     }
-    protected void usePIDOutput (double output){
+
+    protected void usePIDOutput(double output) {
         //for motor
         try {
             yawMotor.setX(output);
         } catch (Exception e) {
         }
     }
-    
-    public boolean atSetpoint(){
+
+    public boolean atSetpoint() {
         return Math.abs(getPosition() - getSetpoint()) < 5;
     }
 }
-
-
