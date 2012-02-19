8df5efe0ca732a9a7c40989fc176149a4f13658c
Ed Venator <esv@case.edu>
2012-02-18 20:02:57 -0500

    Moved encoder from 7 to 4 so they're next to each other.

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -34,7 +34,7 @@ public class RobotMap {
     public static final int leftEncoderA = 1;
     public static final int leftEncoderB = 2;
     public static final int rightEncoderA = 3;
-    public static final int rightEncoderB = 7;
+    public static final int rightEncoderB = 4;
     public static final int pitchEncoder = 1;
     public static final int yawEncoderA = 13;
     public static final int yawEncoderB = 14;
