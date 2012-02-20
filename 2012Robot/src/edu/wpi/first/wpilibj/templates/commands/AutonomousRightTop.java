3016043acab44db04389e53937d6151c21e102f6
Gillie Rosen <puella.noctis@gmail.com>
2012-02-20 11:21:32 -0500

    added AutonomousRightTop, Right, and Bottom

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousRightTop.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousRightTop.java
@@ -9,30 +9,30 @@ import edu.wpi.first.wpilibj.templates.commands.ShootBall;
 import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.templates.commands.ShmackDown;
 
-
 /**
- *
- * @author bradmiller
+ * A Command that extends CommandGroup to make an Autonomous program for the right position.
+ * @author Emma and Jessie
  */
-public class AutonomousLeftLeft extends CommandGroup {
+public class AutonomousRightTop extends CommandGroup {
 
-    public AutonomousLeftLeft() {
+    /**
+     * Creates an Autonomous Command for the right position that runs through the following steps:
+     * aim at the top target, shoot twice, back up 20 in, turn 45 clockwise, 
+     * move backward 64 in, turn 45 clockwise, move backward 27 in, and smack down the ramp.
+     */
+    public AutonomousRightTop() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-        addSequential(new AutonomousAimStart());
-        addParallel(new Aim(3));
-        addSequential(new ShootOn());
-        addSequential(new ShootBall());
-        addSequential(new ShootBall());
-        addSequential(new ShootOff());
+        
+        addSequential(new AutonomousStopTop());
+        
         addSequential(new Go(-20.0, 0.5));
-        //counter clockwise
-        addSequential(new Spinning(DriveTrain.Turn45Fast, 0.5));
-        addSequential(new Go(-74.0, 0.5));
         //clockwise
         addSequential(new Spinning(DriveTrain.TurnNeg45Fast, 0.5));
+        addSequential(new Go(-64.0, 0.5));
+        //counter clockwise
+        addSequential(new Spinning(DriveTrain.Turn45Fast, 0.5));
         addSequential(new Go(-27.0, 0.5));
         addSequential(new ShmackDown());
     }
 }
-    
