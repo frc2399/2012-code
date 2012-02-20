198ecc2667596d22cc289ea4fa05fabde9d51bbf
Gillie Rosen <puella.noctis@gmail.com>
2012-02-20 11:10:13 -0500

    AutonomousStop and its left, right, top, bottom added

---
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousStopLeft.java
@@ -0,0 +1,34 @@
+
+package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.templates.commands.Go;
+import edu.wpi.first.wpilibj.templates.commands.Spinning;
+import edu.wpi.first.wpilibj.templates.commands.Aim;
+import edu.wpi.first.wpilibj.templates.commands.LoadBall;
+import edu.wpi.first.wpilibj.templates.commands.ShootBall;
+import edu.wpi.first.wpilibj.templates.subsystems.DriveTrain;
+import edu.wpi.first.wpilibj.templates.commands.ShmackDown;
+import edu.wpi.first.wpilibj.templates.commands.AutonomousAimStart;
+
+/** 
+ * A Command that extends CommandGroup to make an Autonomous program for the center position.
+ * @author Emma and Jessie
+ */
+public class AutonomousStopLeft extends CommandGroup {
+
+    /**
+     * Creates an Autonomous Command for the center position that runs through the following steps:
+     * aim at the top target, shoot twice, back up 30 in, turn 180, move forward 30 in, and smack down the ramp.
+     */
+    public AutonomousStopLeft() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        addSequential(new AutonomousAimStart());
+        addParallel(new Aim(3));
+        addSequential(new ShootOn());
+        addSequential(new ShootBall());
+        addSequential(new ShootBall());
+        addSequential(new ShootOff());
+        
+    }
+}
