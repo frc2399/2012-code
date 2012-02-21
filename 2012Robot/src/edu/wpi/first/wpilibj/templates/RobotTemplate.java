fb394169c4591dca66eae4cbfa0a53b2ae4f4f7a
Jessie Adkins <zaheelee@gmail.com>
2012-02-21 16:47:31 -0500

    Feeder testing 1.3

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj.templates.commands.CommandBase;
 import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
 import edu.wpi.first.wpilibj.templates.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,11 +34,12 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
+        //autonomousCommand = new ExampleCommand();
         AxisCamera.getInstance().writeBrightness(60);
         AxisCamera.getInstance().writeColorLevel(50);
         // Initialize all subsystems
         CommandBase.init();
+        SmartDashboard.putData("SchedulerData", Scheduler.getInstance());
     }
 
     public void autonomousInit() {
