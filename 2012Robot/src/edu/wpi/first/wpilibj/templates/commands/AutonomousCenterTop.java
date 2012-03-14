a2c4ee7394617548e4db6a5403410140a5fc8443
Jessie Adkins <zaheelee@gmail.com>
2012-03-14 16:28:13 -0400

    fixing autonomous

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousCenterTop.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousCenterTop.java
@@ -17,7 +17,7 @@ public class AutonomousCenterTop extends CommandGroup {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
         
-        //addSequential(new AutonomousStopTop());
+        addSequential(new AutonomousStopTop());
         addSequential(new AutonomousCenterDirections());
     }
 }
