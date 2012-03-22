d7398da74c42068facdd5a816c9da3a3c5624c2c
Ed Venator <esv@case.edu>
2012-03-22 09:37:46 -0400

    Set up the code to run autonomous modes without shooting. (Jessie)

---
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/AutonomousTest.java
@@ -0,0 +1,17 @@
+
+package edu.wpi.first.wpilibj.templates.commands;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ * @author Jessie
+ */
+public class AutonomousTest extends CommandGroup {
+
+    public AutonomousTest() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        addSequential(new AutonomousCenterDirections());
+    }
+
+}
