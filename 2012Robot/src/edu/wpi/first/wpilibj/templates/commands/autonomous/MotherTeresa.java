c40375e1ef6c92c78a33e768770919d71267ffe1
Gillie Rosen <puella.noctis@gmail.com>
2012-03-31 15:17:23 -0400

    ball feeding autonomous updated slightly

--- 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/autonomous/MotherTeresa.java
+++ 2012Robot/src/edu/wpi/first/wpilibj/templates/commands/autonomous/MotherTeresa.java
@@ -10,15 +10,12 @@ import edu.wpi.first.wpilibj.templates.commands.UnloadBall;
 public class MotherTeresa extends CommandGroup {
 
     /**
-     * Creates an Autonomous Command for the center position that runs through the following steps:
-     * aim at the top target, shoot twice, back up 30 in, turn 180, move forward 30 in, and smack down the ramp.
+     * Creates an Autonomous Command that feeds balls out of the robot instead of 
+     * up to the shooter. 
      */
     public MotherTeresa() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-        //addSequential(new FindReset());
-       // addSequential(new AutonomousAimStart());
-      //  addParallel(new Aim(1));
         addParallel(new UnloadBall());
         addParallel(new FeedBallOn(-1, 0.5));
         
